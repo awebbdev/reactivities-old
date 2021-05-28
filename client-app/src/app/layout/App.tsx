9b51d0820183788978893bda1127736c43735319
thebigbadox <adamwebb13@gmail.com>
2021-05-27 21:26:16 -0500

    Added styling and content

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/Activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+
+  const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+        console.log(response);
+        setActivities(response.data);
+      })
+  }, [])
+
+  function handleSelectedActivity(id: string){
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard 
+          activities={activities} 
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectedActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+        />
+      </Container>
+    </Fragment>
+  );
+}
+
+export default App;
