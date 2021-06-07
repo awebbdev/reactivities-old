86316c069b8f951c61fe0bd350d554a2d36c8e7a
Adam R Webb <adamwebb13@gmail.com>
2021-06-06 20:01:17 -0500

    Section10 (#12)
    * Error Handling API and Client
    
    * End of Section 10

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -3,27 +3,35 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Persistence;
+using FluentValidation;
+using Application.Core;
 
 namespace Application.Activities {
     public class Create {
-        public class Command : IRequest {
+        public class Command : IRequest<Result<Unit>> {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command> {
+        public class CommandValidator : AbstractValidator<Command>{
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>> {
             private readonly DataContext _context;
             public Handler (DataContext context) 
             {
                 _context = context;
             }
 
-            public async Task<Unit> Handle (Command request, CancellationToken cancellationToken) 
+            public async Task<Result<Unit>> Handle (Command request, CancellationToken cancellationToken) 
             {
                 _context.Activities.Add(request.Activity);
-
-                await _context.SaveChangesAsync();
-
-                return Unit.Value;
+                return await _context.SaveChangesAsync() > 0 ? 
+                    Result<Unit>.Success(Unit.Value) : 
+                    Result<Unit>.Failure("Failed to create activity");
             }
         }
     }
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,17 +1,26 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
 namespace Application.Activities {
     public class Edit {
-        public class Command : IRequest {
+        public class Command : IRequest<Result<Unit>> {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command> {
+        public class CommandValidator : AbstractValidator<Command> {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>> {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
             public Handler (DataContext context, IMapper mapper) {
@@ -19,14 +28,12 @@ namespace Application.Activities {
                 _context = context;
             }
 
-            public async Task<Unit> Handle (Command request, CancellationToken cancellationToken) {
+            public async Task<Result<Unit>> Handle (Command request, CancellationToken cancellationToken) {
                 var activity = await _context.Activities.FindAsync (request.Activity.Id);
+                if(activity == null) return null;
 
                 _mapper.Map(request.Activity, activity);
-
-                await _context.SaveChangesAsync ();
-
-                return Unit.Value;
+                return await _context.SaveChangesAsync () > 0 ? Result<Unit>.Success(Unit.Value) : Result<Unit>.Failure("Failed to update activity");
             }
         }
     }
